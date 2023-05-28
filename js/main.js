0bd9657dcbc846cbd1d155518945c50950d2b2d1
litocode24 <marcelo_braganza@hotmail.com>
2023-05-28 20:04:43 -0300

    TuTurno_V1.2

---
+++ js/main.js
@@ -0,0 +1,343 @@
+function mostrarDias() {
+
+    let mes = document.getElementById('selectMeses');
+    let valueMes = mes.value;
+
+    if (valueMes == 'enero') {
+
+        document.getElementById('selectDiasEne').style.display = 'flex';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'febrero') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'flex';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'marzo') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'flex';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'abril') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'flex';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'mayo') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'flex';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'junio') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'flex';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'julio') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'flex';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'agosto') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'flex';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'septiembre') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'flex';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'octubre') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'flex';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'noviembre') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'flex';
+
+        document.getElementById('selectDiasDic').style.display = 'none';
+
+    } else if (valueMes == 'diciembre') {
+
+        document.getElementById('selectDiasEne').style.display = 'none';
+
+        document.getElementById('selectDia').style.display = 'none';
+
+        document.getElementById('selectDiasFeb').style.display = 'none';
+
+        document.getElementById('selectDiasMar').style.display = 'none';
+
+        document.getElementById('selectDiasAbr').style.display = 'none';
+
+        document.getElementById('selectDiasMay').style.display = 'none';
+
+        document.getElementById('selectDiasJun').style.display = 'none';
+
+        document.getElementById('selectDiasJul').style.display = 'none';
+
+        document.getElementById('selectDiasAgo').style.display = 'none';
+
+        document.getElementById('selectDiasSep').style.display = 'none';
+
+        document.getElementById('selectDiasOct').style.display = 'none';
+
+        document.getElementById('selectDiasNov').style.display = 'none';
+
+        document.getElementById('selectDiasDic').style.display = 'flex';
+
+    }
+}
